d4d9be7377d01b3a53fe67d111c8868d62bcbafc
thealejandro <thealejandro7w7@gmail.com>
2022-01-13 01:33:20 -0600

    Publish HasProfilePhoto.php and update class for profile photo

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
+use App\Actions\Fortify\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
