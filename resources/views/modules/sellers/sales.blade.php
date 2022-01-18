cb51ea8359603f77db9244ecfe87706303e0baf4
Angel Ponce <acdpf000@gmail.com>
2022-01-18 04:51:59 -0600

    Adding x-module-title and x-module components

--- resources/views/modules/sellers/sales.blade.php
+++ resources/views/modules/sellers/sales.blade.php
@@ -1,15 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Sales') }}
-        </h2>
+        <x-module-title title="Sales"></x-module-title>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+    <x-module>
 
-            </div>
-        </div>
-    </div>
+    </x-module>
 </x-app-layout>
