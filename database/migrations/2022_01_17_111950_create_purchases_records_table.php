95c09df45678f99db8d55588c151158cc95bdfd7
thealejandro <thealejandro7w7@gmail.com>
2022-01-17 22:47:54 -0600

    Add new migrations and model's

---
+++ database/migrations/2022_01_17_111950_create_purchases_records_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases_records', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_of_purchase');
+            $table->json('invoiceDetails');
+            $table->boolean('invoiceCountable');
+            $table->json('purchaseDetails');
+            $table->foreignId('inventoryRecords')->constrained('inventory_records', 'id')->cascadeOnUpdate();
+            $table->boolean('purchaseVerified')->default(FALSE);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchases_records', function (Blueprint $tab) {
+            $tab->dropForeign(['inventoryRecords']);
+        });
+        Schema::dropIfExists('purchases_records');
+    }
+}
