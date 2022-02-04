6a97902f552d2ac50bcfb65404e622259ece89f3
thealejandro <thealejandro7w7@gmail.com>
2022-02-03 23:51:22 -0600

    Add button in view

--- resources/views/components/sales-table.blade.php
+++ resources/views/components/sales-table.blade.php
@@ -7,7 +7,7 @@
             Total: {{ Helper::GTMoney($total) }}
         </div>
     </div>
-    <div class="w-full h-auto" wire:loading>
+    <div class="w-full h-auto text-center" wire:loading>
         <x-line-loader></x-line-loader>
     </div>
 
@@ -15,13 +15,31 @@
         $allUnits = array_map(fn($i) => $i['units'], $itemsStructure);
     @endphp
     @if (!in_array(-1, $allUnits) && count($itemsStructure) > 0)
-        <div class="flex justify-center gap-4" wire:loading.remove>
+        <div class="flex justify-evenly items-center py-2" wire:loading.remove>
             <button class="btn btn-primary" wire:click='sell'>
                 Vender
             </button>
+            <label for="invoiceGenerate" class="flex items-center">
+                <x-jet-checkbox id="invoiceGenerate" wire:model="invoiceGenerate"/>
+                <span class="ml-2 text-sm text-gray-600">Generar Factura</span>
+            </label>
             <button class="btn btn-accent" wire:click='cancel'>
                 Cancelar
             </button>
+        </div>
+        <div class="flex items-center justify-evenly">
+            <div class="flex gap-2 items-center">
+                <x-jet-label for="invoiceNIT" value="NIT" />
+                <x-jet-input id="invoiceNIT" type="text" wire:model="invoiceNIT" />
+            </div>
+            <div class="flex gap-2 items-center">
+                <x-jet-label for="invoiceName" value="Nombre" />
+                <x-jet-input id="invoiceName" type="text" wire:model="invoiceName"/>
+            </div>
+            <div class="flex gap-2 items-center">
+                <x-jet-label for="invoiceAddress" value="Direccion" />
+                <x-jet-input id="invoiceAddress" type="text" wire:model="invoiceAddress"/>
+            </div>
         </div>
     @endif
 
