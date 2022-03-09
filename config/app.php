294d4f6dcc480018232dc8e15fa3722ad4520ea5
thealejandro <thealejandro7w7@gmail.com>
2022-03-09 11:58:21 -0600

    Add Library Rappasoft Laravel livewire Tables for tables in project

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        Rappasoft\LaravelLivewireTables\LaravelLivewireTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
