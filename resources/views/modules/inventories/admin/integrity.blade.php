ed07a2d7d9d3030de50c8488b65711747daa8fc9
thealejandro <thealejandro7w7@gmail.com>
2022-10-08 17:58:43 -0600

    update

--- resources/views/modules/inventories/admin/integrity.blade.php
+++ resources/views/modules/inventories/admin/integrity.blade.php
@@ -6,8 +6,31 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col md:flex-row gap-4 items-center overflow-hidden">
+        <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col gap-4 items-center overflow-hidden">
+
+            <div class="flex justify-between w-full">
+                <select class="select w-full max-w-xs">
+                    <option disabled selected>Pick your favorite Simpson</option>
+                    <option>Homer</option>
+                    <option>Marge</option>
+                    <option>Bart</option>
+                    <option>Lisa</option>
+                    <option>Maggie</option>
+                </select>
+
+                <div class="form-control">
+                    <div class="input-group">
+                        <input type="text" placeholder="Search…" class="input input-bordered" />
+                        <button class="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="flex w-full">
+                </div>
             </div>
         </div>
     </div>
--- resources/views/modules/inventories/admin/see-all.blade.php
+++ resources/views/modules/inventories/admin/see-all.blade.php
@@ -7,8 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col md:flex-row gap-4 items-center overflow-hidden">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            </div>
+            {{-- <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"> --}}
+            {{-- </div> --}}
+           
         </div>
     </div>
 </x-app-layout>
