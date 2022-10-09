8f4b90500b2d61fe38f5e76ec18d3e816d9b04d8
thealejandro <thealejandro7w7@gmail.com>
2022-10-08 18:03:10 -0600

    update

--- resources/views/modules/inventories/admin/integrity.blade.php
+++ resources/views/modules/inventories/admin/integrity.blade.php
@@ -28,17 +28,15 @@
                 </div>
             </div>
 
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white w-full overflow-hidden md:px-6 md:py-6 shadow-xl sm:rounded-lg">
                 <div class="flex flex-col md:flex-row w-full">
                     <div class="overflow-x-auto">
                         <table class="table w-full">
                             <!-- head -->
                             <thead>
                             <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Job</th>
-                                <th>Favorite Color</th>
+                                <th>Codigo</th>
+                                <th>Producto</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -46,22 +44,16 @@
                             <tr>
                                 <th>1</th>
                                 <td>Cy Ganderton</td>
-                                <td>Quality Control Specialist</td>
-                                <td>Blue</td>
                             </tr>
                             <!-- row 2 -->
                             <tr>
                                 <th>2</th>
                                 <td>Hart Hagerty</td>
-                                <td>Desktop Support Technician</td>
-                                <td>Purple</td>
                             </tr>
                             <!-- row 3 -->
                             <tr>
                                 <th>3</th>
                                 <td>Brice Swyre</td>
-                                <td>Tax Accountant</td>
-                                <td>Red</td>
                             </tr>
                             </tbody>
                         </table>
