d82607150811f6a4fb7380f9cb33baa906db0d28
thealejandro <thealejandro7w7@gmail.com>
2022-10-08 18:00:28 -0600

    Update

--- resources/views/modules/inventories/admin/integrity.blade.php
+++ resources/views/modules/inventories/admin/integrity.blade.php
@@ -29,7 +29,43 @@
             </div>
 
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="flex w-full">
+                <div class="flex flex-col md:flex-row w-full">
+                    <div class="overflow-x-auto">
+                        <table class="table w-full">
+                            <!-- head -->
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Desktop Support Technician</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Brice Swyre</td>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
