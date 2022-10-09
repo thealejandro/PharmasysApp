757d5dc896f13ca443a60956538d22f13c79aa8b
thealejandro <thealejandro7w7@gmail.com>
2022-10-08 18:35:37 -0600

    update

--- resources/views/modules/inventories/admin/integrity.blade.php
+++ resources/views/modules/inventories/admin/integrity.blade.php
@@ -10,7 +10,7 @@
 
             <div class="flex justify-between w-full">
                 <select class="select w-full max-w-xs">
-                    <option disabled selected>Pick your favorite Simpson</option>
+                    <option disabled selected>Sucursal</option>
                     <option>Homer</option>
                     <option>Marge</option>
                     <option>Bart</option>
@@ -20,9 +20,11 @@
 
                 <div class="form-control">
                     <div class="input-group">
-                        <input type="text" placeholder="Search…" class="input input-bordered" />
+                        <input type="text" placeholder="{{ __("Search item...") }}" class="input input-bordered" />
                         <button class="btn btn-square">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
                         </button>
                     </div>
                 </div>
@@ -87,6 +89,146 @@
                             </div>
                             <button class="btn btn-info w-full">Adjust</button>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Envios --}}
+            <div class="bg-white w-full overflow-hidden md:px-6 md:py-6 shadow-xl sm:rounded-lg">
+                <div class="flex flex-col md:flex-row w-full">
+                    <div class="w-full flex flex-col md:justify-between gap-2">
+                        <div class="overflow-x-auto">
+                            <table class="table w-full">
+                                <!-- head -->
+                                <thead>
+                                <tr>
+                                    <th>Registro</th>
+                                    <th>Fecha salida</th>
+                                    <th>Fecha recibido</th>
+                                    <th>Cantidad</th>
+                                    <th>Envio</th>
+                                    <th>Recibio</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <!-- row 1 -->
+                                <tr>
+                                    <th>1</th>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                </tr>
+                                <!-- row 2 -->
+                                <tr>
+                                    <th>2</th>
+                                    <td>Hart Hagerty</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                </tr>
+                                <!-- row 3 -->
+                                <tr>
+                                    <th>3</th>
+                                    <td>Brice Swyre</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="flex flex-col md:flex-row gap-3 w-full justify-between">
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">
+                                        Edit quantity registry for shipping
+                                    </span>
+                                </label>
+                                <label class="input-group input-group-md">
+                                    <span>{{ __("Number") }}</span>
+                                    <input type="text" placeholder="0" class="input input-bordered input-md" />
+                                </label>
+                            </div>
+
+                            <div class="stats stats-vertical lg:stats-horizontal shadow">
+                                <div class="stat">
+                                    <div class="stat-title">{{ __("Shipments") }}</div>
+                                    <div class="stat-value">31</div>
+                                    {{-- <div class="stat-desc">Jan 1st - Feb 1st</div> --}}
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-info w-full">Adjust</button>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Ventas --}}
+            <div class="bg-white w-full overflow-hidden md:px-6 md:py-6 shadow-xl sm:rounded-lg">
+                <div class="flex flex-col md:flex-row w-full">
+                    <div class="w-full flex flex-col md:justify-between gap-2">
+                        <div class="overflow-x-auto">
+                            <table class="table w-full">
+                                <!-- head -->
+                                <thead>
+                                <tr>
+                                    <th>Registro</th>
+                                    <th>Fecha Venta</th>
+                                    <th>Cantidad</th>
+                                    <th>Vendedor</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <!-- row 1 -->
+                                <tr>
+                                    <th>1</th>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                </tr>
+                                <!-- row 2 -->
+                                <tr>
+                                    <th>2</th>
+                                    <td>Hart Hagerty</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                </tr>
+                                <!-- row 3 -->
+                                <tr>
+                                    <th>3</th>
+                                    <td>Brice Swyre</td>
+                                    <td>Cy Ganderton</td>
+                                    <td>Cy Ganderton</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="flex flex-col md:flex-row gap-3 w-full justify-between">
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">
+                                        Edit quantity registry for shipping
+                                    </span>
+                                </label>
+                                <label class="input-group input-group-md">
+                                    <span>{{ __("Number") }}</span>
+                                    <input type="text" placeholder="0" class="input input-bordered input-md" />
+                                </label>
+                            </div>
+
+                            <div class="stats stats-vertical lg:stats-horizontal shadow">
+                                <div class="stat">
+                                    <div class="stat-title">{{ __("Shipments") }}</div>
+                                    <div class="stat-value">31</div>
+                                    {{-- <div class="stat-desc">Jan 1st - Feb 1st</div> --}}
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-info w-full">Adjust</button>
                     </div>
                 </div>
             </div>
