f08675147dfeb0b22d2fbd19406ea436c9a183a7
thealejandro <thealejandro7w7@gmail.com>
2022-10-10 23:20:10 -0600

    Update view

--- resources/views/modules/inventories/admin/inventories.blade.php
+++ resources/views/modules/inventories/admin/inventories.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col md:flex-row gap-4 items-center overflow-hidden">
+        <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col md:flex-row gap-4 justify-center overflow-hidden">
             <div class="flex flex-col bg-white w-full px-4 py-4 overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="flex flex-col md:flex-row w-full justify-between">
                     <select class="select select-ghost w-full max-w-xs">
