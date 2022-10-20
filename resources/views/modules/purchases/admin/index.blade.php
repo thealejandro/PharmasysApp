44635d37c7db1ffaf69f3b602b7859676755c587
thealejandro <thealejandro7w7@gmail.com>
2022-10-19 18:45:30 -0600

    Update view

--- resources/views/modules/purchases/admin/index.blade.php
+++ resources/views/modules/purchases/admin/index.blade.php
@@ -65,27 +65,13 @@
                     </div>
                     <div class="flex-1 w-full card bg-base-100 shadow-md">
                         <div class="card-body items-center text-center">
-                            <h2 class="card-title">{{ __("See all") }}</h2>
-                            <p>{{ __("Unbalanced product verification module in store inventories") }}</p>
                             <div class="card-actions">
-                            <a href="{{ route('module.inventories.admin.stores.seeall') }}">
-                                <button class="btn btn-primary">
-                                    {{ __("Go") }}
-                                </button>
-                            </a>
                             </div>
                         </div>
                     </div>
                     <div class="flex-1 w-full card bg-base-100 shadow-md">
                         <div class="card-body items-center text-center">
-                            <h2 class="card-title">{{ __("See all") }}</h2>
-                            <p>{{ __("Unbalanced product verification module in store inventories") }}</p>
                             <div class="card-actions">
-                            <a href="{{ route('module.inventories.admin.stores.seeall') }}">
-                                <button class="btn btn-primary">
-                                    {{ __("Go") }}
-                                </button>
-                            </a>
                             </div>
                         </div>
                     </div>
