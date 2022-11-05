0dd63271cb8c76d476f7d4fb72cac3319c630b0c
thealejandro <thealejandro7w7@gmail.com>
2022-11-05 14:25:06 -0600

    Update view

--- resources/views/modules/purchases/admin/records.blade.php
+++ resources/views/modules/purchases/admin/records.blade.php
@@ -7,76 +7,95 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col gap-4 md:gap-6 object-center overflow-hidden">
-            <div class="flex flex-col md:flex-row gap-4 md:gap-6 bg-white w-full overflow-hidden items-center justify-center p-5 shadow-md sm:rounded-lg">
+            <div class="flex flex-col gap-4 md:gap-6 bg-white w-full overflow-hidden items-center justify-center p-5 shadow-md sm:rounded-lg">
                 {{-- <div class="flex flex-col md:flex-row gap-4 md:gap-6 items-center justify-center"> --}}
-                    {{-- proveedor --}}
-                    <div class="flex-1 form-control">
-                        <label class="label">
-                            <span class="label-text-alt">Proveedor</span>
-                        </label>
-                        <select class="select select-bordered">
-                            <option disabled selected>Seleccione un proveedor</option>
-                            <option>Star Wars</option>
-                            <option>Harry Potter</option>
-                            <option>Lord of the Rings</option>
-                            <option>Planet of the Apes</option>
-                            <option>Star Trek</option>
-                        </select>
+
+                    <div class="flex flex-col md:flex-row gap-4 md:gap-6 w-full items-center">
+                        {{-- proveedor --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text-alt">Proveedor</span>
+                            </label>
+                            <select class="select select-bordered">
+                                <option disabled selected>Seleccione un proveedor</option>
+                                <option>Star Wars</option>
+                                <option>Harry Potter</option>
+                                <option>Lord of the Rings</option>
+                                <option>Planet of the Apes</option>
+                                <option>Star Trek</option>
+                            </select>
+                        </div>
+
+                        {{-- numero --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text">Numero de factura</span>
+                            </label>
+                            <input type="text" placeholder="Numero de factura" class="input input-bordered w-full max-w-xs" />
+                        </div>
+
+                        {{-- Codigo/nombre del item --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text">Codigo/Nombre de producto</span>
+                            </label>
+                            <input type="text" placeholder="Codigo/Nombre de producto" class="input input-bordered w-full max-w-xs" />
+                        </div>
+
+                        {{-- categoria --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text-alt">Categoria</span>
+                            </label>
+                            <select class="select select-bordered">
+                                <option disabled selected>Seleccione una categoria</option>
+                                <option>Star Wars</option>
+                                <option>Harry Potter</option>
+                                <option>Lord of the Rings</option>
+                                <option>Planet of the Apes</option>
+                                <option>Star Trek</option>
+                            </select>
+                        </div>
                     </div>
 
-                    {{-- numero --}}
-                    <div class="flex-1 form-control">
-                        <label class="label">
-                            <span class="label-text">Numero de factura</span>
-                        </label>
-                        <input type="text" placeholder="Numero de factura" class="input input-bordered w-full max-w-xs" />
+                    <div class="flex flex-col md:flex-row gap-4 md:gap-6 w-full items-center">
+                        {{-- marca --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text-alt">Marca</span>
+                            </label>
+                            <select class="select select-bordered">
+                                <option disabled selected>Seleccione una marca</option>
+                                <option>Star Wars</option>
+                                <option>Harry Potter</option>
+                                <option>Lord of the Rings</option>
+                                <option>Planet of the Apes</option>
+                                <option>Star Trek</option>
+                            </select>
+                        </div>
+
+                        {{-- fecha --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text">Desde</span>
+                            </label>
+                            <input type="date" class="input input-bordered w-full max-w-xs" />
+                        </div>
+
+                        {{-- fecha --}}
+                        <div class="flex-1 form-control justify-center">
+                            <label class="label">
+                                <span class="label-text">Hasta</span>
+                            </label>
+                            <input type="date" class="input input-bordered w-full max-w-xs" />
+                        </div>
+
+                        <button class="btn btn-info">
+                            search
+                        </button>
                     </div>
 
-                    {{-- Codigo/nombre del item --}}
-                    <div class="flex-1 form-control">
-                        <label class="label">
-                            <span class="label-text">Codigo/Nombre de producto</span>
-                        </label>
-                        <input type="text" placeholder="Codigo/Nombre de producto" class="input input-bordered w-full max-w-xs" />
-                    </div>
 
-                    {{-- categoria --}}
-                    <div class="flex-1 form-control">
-                        <label class="label">
-                            <span class="label-text-alt">Categoria</span>
-                        </label>
-                        <select class="select select-bordered">
-                            <option disabled selected>Seleccione una categoria</option>
-                            <option>Star Wars</option>
-                            <option>Harry Potter</option>
-                            <option>Lord of the Rings</option>
-                            <option>Planet of the Apes</option>
-                            <option>Star Trek</option>
-                        </select>
-                    </div>
-
-                    {{-- marca --}}
-                    <div class="flex-1 form-control">
-                        <label class="label">
-                            <span class="label-text-alt">Marca</span>
-                        </label>
-                        <select class="select select-bordered">
-                            <option disabled selected>Seleccione una marca</option>
-                            <option>Star Wars</option>
-                            <option>Harry Potter</option>
-                            <option>Lord of the Rings</option>
-                            <option>Planet of the Apes</option>
-                            <option>Star Trek</option>
-                        </select>
-                    </div>
-
-                    {{-- fecha --}}
-                    <div class="flex-1 form-control">
-                        <label class="label">
-                            <span class="label-text">Fecha de factura</span>
-                        </label>
-                        <input type="date" class="input input-bordered w-full max-w-xs" />
-                    </div>
                 {{-- </div> --}}
             </div>
 
