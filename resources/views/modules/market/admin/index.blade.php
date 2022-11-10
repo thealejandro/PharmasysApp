595e282d3bbb62994b3523afe9c56b7d89b295b3
thealejandro <thealejandro7w7@gmail.com>
2022-11-10 14:25:13 -0600

    add view index of admin of market and sell

---
+++ resources/views/modules/market/admin/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Sell') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col gap-4 md:gap-6 object-center overflow-hidden">
+            <div class="flex flex-col gap-4 md:gap-6 bg-white w-full overflow-hidden items-center justify-center p-5 shadow-md sm:rounded-lg">
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
