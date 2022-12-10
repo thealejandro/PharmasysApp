0bf046418a5c1a305521a5f320e757ff781ce1fd
thealejandro <thealejandro7w7@gmail.com>
2022-12-10 15:28:51 -0600

    Update route of view

--- routes/web.php
+++ routes/web.php
@@ -110,6 +110,6 @@ Route::view('market/invoice/record', 'modules/market/seller/recordOfInvoices')->
 
 Route::view('admin/settings/editOrAdd', 'modules/edit/index')->name("module.settings.editoradd.index");
 Route::view('admin/settings/editOrAdd/users', 'modules/edit/users/index')->name("module.settings.editoradd.users.index");
-Route::view('admin/settings/editOrAdd/inventories', 'modules/edit/inventories/index')->name("module.settings.editoradd.inventories.index");
+Route::view('admin/settings/editOrAdd/inventories', 'modules/edit/inventory/index')->name("module.settings.editoradd.inventories.index");
 Route::view('admin/settings/editOrAdd/items', 'modules/edit/items/index')->name("module.settings.editoradd.items.index");
 Route::view('admin/settings/editOrAdd/telephonies', 'modules/edit/telephonies/index')->name("module.settings.editoradd.telephonies.index");
