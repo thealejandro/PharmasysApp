e5137d1af1e84da4b0909160448d919eca7dce15
thealejandro <thealejandro7w7@gmail.com>
2022-12-10 15:29:09 -0600

    Add style in views

---
+++ resources/views/modules/edit/telephonies/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Telephonies') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto px-6 lg:px-8 md:py-8 py-4 flex flex-col md:flex-row gap-4 object-center overflow-hidden">
+            
+        </div>
+    </div>
+</x-app-layout>
