b930e9dd922ec47ebcd295ded74556237e2de61f
thealejandro <thealejandro7w7@gmail.com>
2023-01-01 21:51:05 -0600

    Update views

--- resources/views/modules/edit/index.blade.php
+++ resources/views/modules/edit/index.blade.php
@@ -11,20 +11,6 @@
 
                 <div class="flex-1 w-full card bg-base-100 shadow-md h-56">
                     <div class="card-body items-center text-center">
-                        <h2 class="card-title">{{ __("Edit Inventory") }}</h2>
-                        <p>{{ __("Edit data for item on inventory for stores.") }}</p>
-                        <div class="card-actions">
-                        <a href="{{ route('module.settings.editoradd.inventories.index') }}">
-                            <button class="btn btn-primary">
-                                {{ __("Go") }}
-                            </button>
-                        </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="flex-1 w-full card bg-base-100 shadow-md h-56">
-                    <div class="card-body items-center text-center">
                         <h2 class="card-title">{{ __("Edit Items") }}</h2>
                         <p>{{ __("") }}</p>
                         <div class="card-actions">
