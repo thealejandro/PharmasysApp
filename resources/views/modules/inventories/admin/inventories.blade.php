c36b513a3c59e7cb5342ca8e987be8b4ba7d54f1
thealejandro <thealejandro7w7@gmail.com>
2023-11-06 18:42:14 -0600

    Update view

--- resources/views/modules/inventories/admin/inventories.blade.php
+++ resources/views/modules/inventories/admin/inventories.blade.php
@@ -4,12 +4,6 @@
     </x-slot>
 
     <section class="pt-4 lg:max-w-[90%] w-full mx-auto">
-        <div
-            class="flex flex-col gap-4 px-6 py-4 mx-auto overflow-hidden lg:px-8 md:py-8 md:flex-row">
-
             <livewire:inventories.admin.choose-items />
-
-            <livewire:inventories.admin.update-items />
-        </div>
     </section>
 </x-app-layout>
